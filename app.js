cd646bbeea0fe3959158dd82d4bb79bec3e8f643
Archanaa Sathyanarayanan <Archanaa Sathyanarayanan>
2017-01-13 15:24:48 -0600

    customDirectiveAddedForWeatherResults

--- app.js
+++ app.js
@@ -62,3 +62,18 @@ myWeatherApp.controller('forecastController', ['$scope', '$resource', '$routePar
     }
     
 }]);
+
+//CUSTOM DIRECTIVE
+myWeatherApp.directive('weatherResult', function() {
+   return {
+       restrict: 'E',
+       templateUrl: 'directives/weatherResult.html',
+       replace: true,
+       scope: {
+            weatherObject: "=",
+            convertToStandard: "&",
+            convertDate: "&",
+            dateFormat: "@"
+       }
+   } 
+});
